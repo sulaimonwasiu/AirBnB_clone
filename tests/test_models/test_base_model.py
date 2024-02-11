a411e3aaeaf4eccf9a6cee996e6535a7a191204f
Wasiu Sulaimon <sulaimonwasiu13@gmail.com>
2024-02-11 15:25:58 +0300

    clean up

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,18 +1,28 @@
-#!/usr/bin/python3
-"""Unittests for the base class
-"""
-
-import sys
-import os
 import unittest
 from datetime import datetime
+from models import *
 
 
-class TestBaseModel(unittest.TestCase):
-    """Test for the base class
+class Test_BaseModel(unittest.TestCase):
     """
-    pass
+    Test the base model class
+    """
+
+    def setUp(self):
+        self.model1 = BaseModel()
+
+        test_args = self.model1.to_dict()
+        self.model2 = BaseModel(**test_args)
+        print(self.model1.id)
+        print(self.model2.id)
+
+    def test_instantiation(self):
+        self.assertIsInstance(self.model1, BaseModel)
+        self.assertTrue(hasattr(self.model1, "created_at"))
+        self.assertTrue(hasattr(self.model1, "id"))
+        self.assertTrue(hasattr(self.model1, "updated_at"))
+        self.assertDictEqual(self.model1.to_dict(), self.model2.to_dict())
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
